c8725b64f5c886e38f6a3f6c83bef3f5163da1e8
xsui <284898541@qq.com>
2020-06-04 22:01:46 +0800

    some fixes

--- flutter_leave_lc/lib/WeeklySummary.dart
+++ flutter_leave_lc/lib/WeeklySummary.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
+import 'Leavepage.dart';
+import 'MyLeaves.dart';
+import 'TodayLeaves.dart';
 
+class WeeklyTabBarPage extends StatefulWidget {
+  WeeklyTabBarPage({Key key}) : super(key: key);
+  _WeeklyTabBarPageState createState() => _WeeklyTabBarPageState();
+}
 
-class WeeklySummaryPage extends StatelessWidget {
+class _WeeklyTabBarPageState extends State<WeeklyTabBarPage>
+    with SingleTickerProviderStateMixin {
+  TabController _tabController;
+  List _tabs = ["写周报", "周报汇总",];
+  List<Widget> _pages = [
+    LeavePage(),
+    MyLeavesPage(),
+    TodayLeavesPage(),
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: this._pages.length, vsync: this);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        body: new Center(
-          child: new Text('WeeklySummaryPage'),
+    return Scaffold(
+      appBar: new AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0, // 隐藏阴影
+        //flexibleSpace 用于 Tabbar 去除 AppBar 行高
+        flexibleSpace: new Column(
+          children: <Widget>[
+            new TabBar(
+                controller: _tabController,
+                labelColor: Colors.blue,
+                indicatorColor: Colors.blue,
+                tabs: this._tabs.map((e) => Tab(text: e)).toList())
+          ],
         ),
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: _pages,
       ),
     );
   }
